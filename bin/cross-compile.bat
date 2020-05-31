e06082b80b191dc4332b5906e74093784a0eb8ff
mawaya <66057718+mawaya@users.noreply.github.com>
2020-05-31 17:10:33 +0800

    no message

--- bin/cross-compile.bat
+++ bin/cross-compile.bat
@@ -1,1 +1,1 @@
-go run bin\cross-compile.go --compile-only --parallel=2 --include=".*(windows|linux).*amd64" %1
+go run bin\cross-compile.go --compile-only --parallel=2 --include=".*(windows|linux|darwin).*amd64" %1
